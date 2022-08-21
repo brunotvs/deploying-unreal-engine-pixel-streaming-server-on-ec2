a06420ee503125c6e5f76a325ad8e5ef8963b1b6
Bruno Thiago Rego Valeriano Silva <brunothiagorvs@gmail.com>
2022-08-21 17:29:20 -0300

    Changed UE4 for UE

--- UE4-Pixel-Streamer-Bootstrap.ps1
+++ UE4-Pixel-Streamer-Bootstrap.ps1
@@ -15,7 +15,7 @@ You will need to modify the $buildExecutable variable below to match the file na
 $buildExecutable = "Holus.exe"
 $basePath = "C:\PixelStreamer\Downloads"
 
-Write-Output "Starting UE4-Pixel-Streamer-Bootstrap.ps1 from:", $basePath
+Write-Output "Starting UE-Pixel-Streamer-Bootstrap.ps1 from:", $basePath
 
 # Create basePath if unless it already exists.
 if (!(Test-Path -Path $basePath )) {
@@ -28,33 +28,33 @@ Start-Process -FilePath "$basePath\node-v12.8.1-x64.msi" -ArgumentList "/quiet" 
 $env:Path += ";C:\Program Files\nodejs\;C:\Users\Administrator\AppData\Roaming\npm"
 Write-Output "Node.js Installed"
 
-Write-Output "Adding Windows Firewall Rules for UE4 Pixel Streaming"
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 80
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 443
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 19302-19303
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8888
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 8888
-New-NetFirewallRule -DisplayName 'UE4 Pixel Streamer' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 19302-19303
-Write-Output "Windows Firewall Rules Added for UE4 Pixel Streaming"
+Write-Output "Adding Windows Firewall Rules for UE Pixel Streaming"
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 80
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 443
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 19302-19303
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8888
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 8888
+New-NetFirewallRule -DisplayName 'UE Pixel Streamer' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 19302-19303
+Write-Output "Windows Firewall Rules Added for UE Pixel Streaming"
 
-Write-Output "Beginning UE4 Project Extraction"
+Write-Output "Beginning UE Project Extraction"
 Expand-Archive -Force -LiteralPath "$basePath\PixelStreamerProject.zip" -DestinationPath C:\PixelStreamer
-Write-Output "UE4 Project Downloaded and Extracted"
+Write-Output "UE Project Downloaded and Extracted"
 
-Write-Output "Install UE4 Prerequisites"
+Write-Output "Install UE Prerequisites"
 Install-WindowsFeature NET-Framework-Core
-Start-Process -FilePath "C:\PixelStreamer\Windows\Engine\Extras\Redist\en-us\UEPrereqSetup_x64.exe" -ArgumentList "/passive /quiet /norestart /log C:\PixelStreamer\Windows\Engine\Extras\Redist\en-us\UE4PreReqInstall.log" -Wait
-Write-Output "UE4 Prerequisites Installed"
+Start-Process -FilePath "C:\PixelStreamer\Windows\Engine\Extras\Redist\en-us\UEPrereqSetup_x64.exe" -ArgumentList "/passive /quiet /norestart /log C:\PixelStreamer\Windows\Engine\Extras\Redist\en-us\UEPreReqInstall.log" -Wait
+Write-Output "UE Prerequisites Installed"
 
 # Run Pixel Streamer as SYSTEM and on startup
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $action = New-ScheduledTaskAction -Execute "C:\PixelStreamer\Windows\$buildExecutable" -Argument "-PixelStreamingIP=localhost -PixelStreamingPort=8888 -RenderOffScreen"
 $trigger = New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "UE4PixelStreamer-Project-Launch" -Description "UE4PixelStreamer-Project-Launch"
+Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "UEPixelStreamer-Project-Launch" -Description "UEPixelStreamer-Project-Launch"
 
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\PixelStreamer\Windows\Samples\PixelStreaming\WebServers\SignallingWebServer\platform_scripts\cmd\Start_SignallingServer.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "UE4PixelStreamer-SignallingServer-Launch" -Description "UE4PixelStreamer-SignallingServer-Launch"
+Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "UEPixelStreamer-SignallingServer-Launch" -Description "UEPixelStreamer-SignallingServer-Launch"
 
-Write-Output "UE4-Pixel-Streamer-Bootstrap.ps1 Complete"
+Write-Output "UE-Pixel-Streamer-Bootstrap.ps1 Complete"
